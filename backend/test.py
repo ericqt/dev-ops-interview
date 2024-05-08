61112f16d2aac8f622ca43b0fde2e3d431e945bc
Gabe Klee <gabe.t.klee@gmail.com>
2024-05-08 15:51:12 -0400

    Add file upload support

--- backend/test.py
+++ backend/test.py
@@ -1,1 +1,6 @@
-12
+import requests
+
+url = 'http://localhost:3333/upload'
+file = {'file': open('../Interview/AMAZON COM INC | 8-K (February 02, 2023).html', 'rb')}
+resp = requests.post(url=url, files=file) 
+print(resp.json())
