5056316ef5d28c2036af570e0aef6ed0740d02ea
Gabe Klee <gabe.t.klee@gmail.com>
2024-05-08 15:30:28 -0400

    Working search and index

--- migrations/01_init.sql
+++ migrations/01_init.sql
@@ -1,11 +1,7 @@
 CREATE SCHEMA if not exists core;
 
-create table if not exists core.users (
+create table if not exists core.chunks (
 	id serial primary key not null,
-	name varchar(254) not null,
-	email varchar(254) not null,
-	phone_number varchar(254) not null,		
-	picture_url varchar(254) not null,
-	registration_at timestamptz not null,
+	content VARCHAR(32512) not null,
 	created_at timestamptz default current_timestamp not null
 );
